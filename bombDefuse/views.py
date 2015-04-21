590a0f7dc15e3a979a2e38abca658d753b44806d
brett55 <bjcull2@gmail.com>
2015-04-21 15:12:41 -0400

    clean up

---
+++ bombDefuse/views.py
@@ -0,0 +1,16 @@
+import models
+# import forms
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.core.context_processors import csrf
+from django.core import serializers
+from django.template import Context, Template
+from django.contrib.auth.models import User
+from django.utils.html import escape
+from django.db.models.loading import get_model
+from django.http import QueryDict
+
+
+def test(request):
+	return HttpResponse("Hello W0rld 2")
+
--- composeexample/urls.py
+++ composeexample/urls.py
@@ -1,10 +1,7 @@
-from django.conf.urls import include, url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'composeexample.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('bombDefuse.urls', namespace="bombDefuse"))
 ]
