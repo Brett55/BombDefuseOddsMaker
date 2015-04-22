2969d6501eb6abe80c8ee640b83bb6c9f42d3f75
brett55 <bjcull2@gmail.com>
2015-04-22 19:48:15 -0400

    starting fib alg

--- composeexample/urls.py
+++ composeexample/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('bombDefuse.urls', namespace="bombDefuse"))
+    url(r'', include('fibonacciGettR.urls', namespace="fibonacciGettR"))
 ]
