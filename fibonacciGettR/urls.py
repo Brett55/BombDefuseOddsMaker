644bb45d4fd6c7553f3dd656f88ea6ec2998eab7
brett55 <bjcull2@gmail.com>
2015-04-23 19:35:23 -0400

    ajax working

--- fibonacciGettR/urls.py
+++ fibonacciGettR/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^$', 'fibonacciGettR.views.main'),
-    url(r'^calculate/v1/(\d+)/$', 'fibonacciGettR.views.calculate')
+    url(r'^v1/calculate/(\d+)/$', 'fibonacciGettR.views.calculate')
 )
--- fibonacciGettR/views.py
+++ fibonacciGettR/views.py
@@ -2,22 +2,27 @@ import models
 # import forms
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
-from django.template import Context, Template
 from django.utils.html import escape
 
 
 def fib(n):
+    '''return index of nth position in fibonacci sequence'''
+
     seed_val_1, seed_val_2 = 1, 1
-    for _ in xrange(n):
+    for _ in xrange(int(n)):
         yield seed_val_1
         seed_val_1, seed_val_2 = seed_val_2, seed_val_1 + seed_val_2
 
 
 def main(request):
+    '''main landing page'''
+
     return render_to_response('fibonacciGettR/index.html')
 
 
 def calculate(request, n):
-    result = list(fib(n))
-    return HttpResponse("It worked!")
+    '''calculate API, send input to fib function and render result to client'''
+
+    result = list(fib(escape(n)))[-1]
+    return HttpResponse(result)
 
