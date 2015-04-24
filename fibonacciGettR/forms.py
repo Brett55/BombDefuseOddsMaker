a7abc1978857ea4b4f93227af763506a2633cd03
brett55 <bjcull2@gmail.com>
2015-04-23 21:01:56 -0400

    working, except for POST

---
+++ fibonacciGettR/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+import models
+
+
+class Fibonacci(ModelForm):
+    class Meta:
+        model = models.Fibonacci
+        fields = ('user_input', 'result')
--- fibonacciGettR/views.py
+++ fibonacciGettR/views.py
@@ -1,28 +1,47 @@
 import models
-# import forms
+import forms
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.utils.html import escape
 
 
-def fib(n):
+def fib(n, seed_val_1, seed_val_2):
     '''return index of nth position in fibonacci sequence'''
 
-    seed_val_1, seed_val_2 = 1, 1
+    seed_val_1 = int(seed_val_1)
+    seed_val_2 = int(seed_val_2)
+
     for _ in xrange(int(n)):
         yield seed_val_1
         seed_val_1, seed_val_2 = seed_val_2, seed_val_1 + seed_val_2
 
 
 def main(request):
-    '''main landing page'''
+    '''main landing page with previous submissions'''
+    args = {}
+    args.update(csrf(request))
+    args['fib_submissions'] = models.Fibonacci.objects.all()
+    return render_to_response('fibonacciGettR/index.html', args)
 
-    return render_to_response('fibonacciGettR/index.html')
 
-
-def calculate(request, n):
+def calculate(request, n, seed_val_1, seed_val_2):
     '''calculate API, send input to fib function and render result to client'''
 
-    result = list(fib(escape(n)))[-1]
-    return HttpResponse(result)
+    if request.method == 'POST':
+        result = list(fib(escape(n), escape(seed_val_1), escape(seed_val_2)))[-1]
+        form_main = forms.Fibonacci(request.POST)
+        if form_main.valid():
+            cleaned_data = form_main.cleaned_data
+            new_fibonacci = models.Fibonacci()
+            new_fibonacci.user_input = cleaned_data['user_input']
+            new_fibonacci.result = cleaned_data['result']
+            new_fibonacci.save()
+            return HttpResponse(result)
+        else:
+            return HttpResponse(form_main.errors)
+
+    else:
+        HttpResponse("Error: Wrong REST Endpoint")
+
+
 
